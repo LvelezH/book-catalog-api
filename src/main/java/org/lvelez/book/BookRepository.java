900ea0aa2c0bad2b126232de31353aa1780558d1
LvelezH <tienemanitas@gmail.com>
2020-02-23 11:34:25 +0100

    Finish book repository implementation

--- src/main/java/org/lvelez/book/BookRepository.java
+++ src/main/java/org/lvelez/book/BookRepository.java
@@ -1,12 +1,17 @@
 package org.lvelez.book;
 
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import io.quarkus.mongodb.ReactiveMongoClient;
 import io.quarkus.mongodb.ReactiveMongoCollection;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import java.util.List;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
+
+import static com.mongodb.client.model.Filters.eq;
 
 @ApplicationScoped
 public class BookRepository {
@@ -19,6 +24,24 @@ public class BookRepository {
 
     public CompletionStage<List<Book>> list(){
         return getCollection().find().toList().run();
+    }
+
+    public CompletionStage<Optional<Book>> findById(String id) {
+        return getCollection().find(eq("_id", id))
+                .findFirst()
+                .run();
+    }
+
+    public CompletionStage<Void> add(Book book){
+        return getCollection().insertOne(book);
+    }
+
+    public CompletionStage<UpdateResult> update(String id, Book book) {
+        return getCollection().replaceOne(eq("_id", id), book);
+    }
+
+    public CompletionStage<DeleteResult> delete(String id) {
+        return getCollection().deleteOne(eq("_id", id));
     }
 
     private ReactiveMongoCollection<Book> getCollection(){
